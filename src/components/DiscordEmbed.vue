78bd2fca051462240631d3e587124ad88aa48ee9
Sanctuary <sanc.pw@gmail.com>
2018-12-22 20:35:43 -0500

    Move date handling filters to a separate file

--- src/components/DiscordEmbed.vue
+++ src/components/DiscordEmbed.vue
@@ -46,20 +46,12 @@
 </template>
 
 <script>
+import { dates } from '../util/filters.js';
+
 export default {
 	name: 'DiscordEmbed',
 
-	filters: {
-		formatDate(value) {
-			if (!(value instanceof Date)) return value;
-			return `${value.getMonth() + 1}/${value.getDate()}/${value.getFullYear()}`;
-		},
-
-		padZeroes(value) {
-			const [month, day, year] = value.split('/');
-			return `${month.padStart(2, 0)}/${day.padStart(2, 0)}/${year}`;
-		},
-	},
+	filters: dates,
 
 	props: {
 		color: String,
---
+++ src/util/filters.js
@@ -0,0 +1,15 @@
+export const dates = {
+	formatDate(value) {
+		if (!(value instanceof Date)) return value;
+		return `${value.getMonth() + 1}/${value.getDate()}/${value.getFullYear()}`;
+	},
+
+	padZeroes(value) {
+		const [month, day, year] = value.split('/');
+		return `${month.padStart(2, 0)}/${day.padStart(2, 0)}/${year}`;
+	},
+};
+
+export default {
+	dates,
+};
