3dc40e3eed3709c96751bd9526c4eaf4ad4c6f02
Sanctuary <sanc.pw@gmail.com>
2019-08-03 19:15:06 -0400

    Remove /demo folderUnnecessary now that /docs is a thing

--- demo/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueDiscordMessage from '../src/index.js';
-
-Vue.use(VueDiscordMessage);
-
-Vue.config.productionTip = false;
-
-new Vue({ render: h => h(App) }).$mount('#app');
