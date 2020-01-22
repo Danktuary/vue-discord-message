9abb51eac7ae53e1430d913404d1737a9b14cfa8
Sanctuary <sanc.pw@gmail.com>
2020-01-21 21:29:50 -0500

    Update profile shortcuts in docs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,17 +6,18 @@ export default ({ Vue }) => {
 			sanc: {
 				author: 'Sanctuary',
 				avatar: 'https://i.imgur.com/0TeacfY.png',
+				roleColor: '#0099ff',
+			},
+			twelve: {
+				author: 'Twelve',
+				avatar: 'https://i.imgur.com/Mc0F0Hf.png',
+				roleColor: '#70f0b4',
 			},
 			rinon: {
 				author: 'Rinon',
 				avatar: 'https://i.imgur.com/axQ9wJl.png',
 				bot: true,
 				roleColor: '#ee82ee',
-			},
-			twelve: {
-				author: 'Twelve',
-				avatar: 'https://i.imgur.com/Mc0F0Hf.png',
-				roleColor: '#15b153',
 			},
 		},
 	});
